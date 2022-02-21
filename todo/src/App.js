3769df1f035752fdb1a36ba85c0433fe880ecc83
Stanislav20 <stas_on38@mail.ru>
2022-02-21 14:58:23 +0800

    lesson_14

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+const word = 'DELL';
+const num = 73;
+const log = false;
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +13,25 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <p>
+          {word}
+        </p>
+        <p>
+          {num}
+        </p>
+        <p style={{
+          fontSize: 54 - 48
+        }}>
+        Hello Hello
+        </p>
+        <p>
+          {log && 'log is true'}
+        </p>
+        <p>
+          {log ? 'log is true' : 'log is false'}
+        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
