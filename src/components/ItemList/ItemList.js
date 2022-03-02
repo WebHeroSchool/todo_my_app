93311768933c725fa9ab6bbd81d911af2c97b13e
Stanislav20 <stas_on38@mail.ru>
2022-03-02 08:40:42 +0800

    lesson 21. add key{}

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Item from '../Item/Item';
 
 const ItemList = ({todoItem}) => (<ul>
-	{todoItem.map (items => <li>)
+	{todoItem.map (items => <li key = {items.value}>)
 		<Item value = {items.value} />
 	</li>)}
 </ul>);
