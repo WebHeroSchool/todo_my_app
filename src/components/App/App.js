234324d7e99a4240d25128f697feab47eaaadff6
Stanislav20 <stas_on38@mail.ru>
2022-03-04 13:22:38 +0800

    lesson 23

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
-import styles from './Ap.module.css';
+import styles from './App.module.css';
 
 const App = () => {
 	const todoItem = [
@@ -14,7 +14,7 @@ const App = () => {
 
 	return (
 	<div className={styles.wrap}>
-  	<h1 className={style.title}>Список дел:</h1>
+  	<h1 className={styles.title}>Список дел:</h1>
     <InputItem />
     <ItemList todoItem = {todoItem} />
     <Footer count = {3}/>
