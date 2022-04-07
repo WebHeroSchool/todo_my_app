dbf63fa9068afd90f1160a2d73783ddea14eb900
Stanislav20 <stas_on38@mail.ru>
2022-04-07 21:00:25 +0800

    убрана ошибка по li

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -3,14 +3,14 @@ import List from '@material-ui/core/List';
 import Item from '../Item/Item';
 
 const ItemList = ({ todoItems, onClickDone }) => (<List>
-  {todoItems.map(item => <li key={item.value}>
+  {todoItems.map(item => <div key={item.value}>
     <Item 
       value={item.value}
       isDone={item.isDone}
       id={item.id}
       onClickDone={onClickDone}
     />
-  </li>)}
+  </div>)}
 </List>);
 
 export default ItemList;
