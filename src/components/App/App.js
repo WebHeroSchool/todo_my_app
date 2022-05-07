b814d74e90263424486e6e7066a450feb2c4131f
Stanislav20 <stas_on38@mail.ru>
2022-05-07 20:54:12 +0800

    default props 3

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -80,7 +80,7 @@ class App extends React.Component {
           onClickDone = {this.onClickDone}
           onClickDelete={this.onClickDelete}
         />
-    		<Footer />
+    		<Footer count={this.state.count} />
   		</div>);
 	}
 }
